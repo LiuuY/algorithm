b5bf8c8222773cc7c18c6512d97934c82bf5b95d
LiuuY <mail4c59@gmail.com>
2021-07-08 16:27:21 +0800

    [priority queue] change

--- priority-queue.js
+++ priority-queue.js
@@ -1,60 +1,103 @@
 /**
  * Priority Queue
- * 最大优先队列
+ * 最小优先队列
  */
 class PriorityQueue {
   #data = [];
   #length = 0;
+  #priorities = new Map();
+
   /**
    * constructor
-   * @param {number[]} data
+   * @param {Function} [comparatorFunction]
    */
-  constructor(data = []) {
-    this.#data = data;
-    this.#length = data.length;
-
-    if (this.#length > 0) {
-      /**
-       * 只需要将非叶子节点移动就可以了，
-       * 因为堆是一个完全二叉树
-       * 1. 总节点数 N 为偶数时，叶子节点数为 N / 2
-       * 2. 总节点数 N 为奇数时，叶子节点数为 (N + 1) / 2
-       */
-      for (let i = (this.#length + 1) >> 1; i >= 0; --i) {
-        this.#sink(i);
-      }
-    }
+  constructor(comparatorFunction) {
+    this.compare = comparatorFunction || this.compare;
   }
 
   /**
+   * insert item to queue.
    * 插入队列
    * 插入到最后，然后上浮（swim） 到正确位置
    * @param {number} item
+   * @param {number} [priority=0] - items priority.
    */
-  insert(item) {
+  insert(item, priority = 0) {
+    this.#priorities.set(item, priority);
     this.#data.push(item);
     this.#swim(this.#length++);
-    console.log(this.#data);
   }
 
   /**
-   * 删除最大值
-   * @return {number}
+   * delete item.
+   * @return {*} [item] - deleted item if not provided will delete top item.
    */
-  delete() {
+  delete(item) {
     if (this.#length === 0) {
       return;
     }
 
-    const top = this.#data[0];
+    const indexToDelete = item ? this.#data.indexOf(item) : 0;
+    const top = this.#data[indexToDelete];
     const bottom = this.#data.pop();
 
     if (--this.#length > 0) {
-      this.#data[0] = bottom;
-      this.#sink(0);
+      this.#data[indexToDelete] = bottom;
+      this.#sink(indexToDelete);
     }
 
+    this.#priorities.delete(top);
     return top;
+  }
+
+  /**
+   * Change priority of the item.
+   * @param {*} item - item we're going to re-prioritize.
+   * @param {number} priority - new item's priority.
+   */
+  changePriority(item, priority) {
+    this.delete(item);
+    this.insert(item, priority);
+  }
+
+  /**
+   * Change priority of the item.
+   * @param {*} item - item we're going to re-prioritize.
+   * @param {number} priority - new item's priority.
+   */
+  changePriority(item, priority) {
+    this.delete(item);
+    this.insert(item, priority);
+  }
+
+  /**
+   * Check if item already exists in a queue.
+   * @param {*} item
+   * @return {boolean}
+   */
+  hasValue(item) {
+    return this.#data.includes(item);
+  }
+
+  /**
+   * Check if queue is empty.
+   * @return {boolean}
+   */
+  get isEmpty() {
+    return this.#length === 0;
+  }
+
+  /**
+   * Compares priorities of two items.
+   * @param {*} a
+   * @param {*} b
+   * @return {number}
+   */
+  compare(a, b) {
+    if (this.#priorities.get(a) === this.#priorities.get(b)) {
+      return 0;
+    }
+    return this.#priorities.get(a) < this.#priorities.get(b) ? -1 : 1;
   }
 
   /**
@@ -64,7 +107,7 @@ class PriorityQueue {
   #swim(k) {
     const p = (k - 1) >> 1;
 
-    while (k > 0 && this.#data[k] > this.#data[p]) {
+    while (k > 0 && this.compare(this.#data[k], this.#data[p]) === -1) {
       this.#swap(this.#data, p, k);
       this.#swim(p);
     }
@@ -84,14 +127,17 @@ class PriorityQueue {
       /**
        * 大顶堆的定义，根大于左子树（j）右子树 （j+1），只需将根与较大的子树对比
        */
-      if (j + 1 <= this.#length && this.#data[j + 1] > this.#data[j]) {
+      if (
+        j + 1 <= this.#length &&
+        this.compare(this.#data[j + 1], this.#data[j]) === -1
+      ) {
         ++j;
       }
 
       /**
        * 如果子树大于根则交换，并需要判断后续的子树 k = j
        */
-      if (this.#data[j] > this.#data[k]) {
+      if (this.compare(this.#data[j], this.#data[k]) === -1) {
         this.#swap(this.#data, j, k);
         k = j;
       } else {
