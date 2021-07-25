fd411f5246090f271bf89b12c99bc0a9b78d5c90
LiuuY <mail4c59@gmail.com>
2021-07-25 17:14:59 +0800

    [longest common subsequence] update

--- longest-common-subsequence.js
+++ longest-common-subsequence.js
@@ -1,11 +1,8 @@
 /**
- * 状态定义：dp[i][j] 为 text1[0..i] 和 text2[0..j] 的最长公共子序列
+ * 状态定义：dp[i][j] 为 text1[0..i] 和 text2[0..j] 的最长公共子序列。
  * 状态转移方程：
  *  1. 当 text1[i] === text2[j] 时，dp[i][j] = dp[i-1][j-1] + 1
  *  2. 当 text1[i] !== text2[j] 时，dp[i][j] = max(dp[i-1][j], dp[i][j-1])
- */
-
-/**
  * @param {string} text1
  * @param {string} text2
  * @return {number}
@@ -30,14 +27,13 @@ const longestCommonSubsequence1 = (text1, text2) => {
   return dp[text1Length][text2Length];
 };
 
+/************************************************************************************/
+
 /**
  * 状态压缩：由于计算 dp[i][j] 只需要 dp[i-1][j-1]（左上角），dp[i-1][j]（左测），
  * dp[i][j-1]（上侧）的值，其中左侧在本次循环计算，上侧还没有被本次循环覆盖，可以只保存
  * 前一行，但是左上角的数据已经被覆盖（在计算 dp[i-1][j] 时），所以需要在每次计算时保存
  * 上侧的值。
- */
-
-/**
  * @param {string} text1
  * @param {string} text2
  * @return {number}
